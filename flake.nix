743c1444bb82984ff324743c82fedfb6376301fd
Joshua Gilman <joshuagilman@gmail.com>
2022-06-06 21:09:43 -0700

    build(flake): updates and cleans up flake

--- flake.nix
+++ flake.nix
@@ -9,19 +9,18 @@
       let
         version = "1.0.0"; # x-release-please-version
 
+        lib = (import ./lib { inherit pkgs lib plugins; });
+        plugins = import ./plugins { inherit pkgs lib; };
+
         # Setup pkgs
         pkgs = import nixpkgs {
           inherit system;
         };
 
-        # Internal attributes
-        lib = self.lib.${system};
-        plugins = import ./plugins { inherit pkgs lib; };
-
-        # Test runner
+        # Import test runner
         runTest = import ./tests/common.nix { inherit pkgs lib plugins; };
 
-        # Helper for aggregating development tools
+        # Helper function for aggregating development tools
         mkTools = tools: (builtins.listToAttrs
           (
             builtins.map
@@ -30,26 +29,9 @@
               tools
           ) // { all = tools; });
 
-        # TODO: Remove when https://github.com/NixOS/nixpkgs/pull/175771 is merged
-        conform = pkgs.buildGoModule rec {
-          pname = "conform";
-          version = "0.1.0-alpha.25";
-
-          src = pkgs.fetchFromGitHub {
-            rev = "v${version}";
-            owner = "siderolabs";
-            repo = "conform";
-            sha256 = "sha256-WgWgigpqPoEBY4tLjbzK02WFwrCWPGQWJ5eakLv5IWw=";
-          };
-
-          vendorSha256 = "sha256-Oigt7tAK4jhBQtfG1wdLHqi11NWu6uJn5fmuqTmR76E=";
-
-          doCheck = false;
-        };
-
         # Define development tools
         tools = mkTools [
-          conform
+          pkgs.conform
           pkgs.cue
           pkgs.just
           pkgs.lefthook
@@ -64,13 +46,13 @@
         configs = import ./.config.nix { inherit tools; };
       in
       rec {
-        # Load lib functions
-        lib = (import ./lib { inherit pkgs lib plugins; });
+        # Expose external API
+        inherit lib;
 
-        # Local tests
+        # Add local tests
         checks = import ./tests { inherit pkgs runTest; };
 
-        # Local development shell
+        # Configure local development shell
         devShells = {
           default = pkgs.mkShell {
             shellHook = (lib.makeAll configs).shellHook;
