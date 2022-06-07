52ac1ce9c476c5bd48177ad498269c1eb35ce6dc
Joshua Gilman <joshuagilman@gmail.com>
2022-06-06 18:28:59 -0700

    refactor: dynamically loads tests from root directory

--- tests/default.nix
+++ tests/default.nix
@@ -1,8 +1,18 @@
 { pkgs, runTest }:
-{
-  conform = pkgs.callPackage ./conform { inherit runTest; };
-  just = pkgs.callPackage ./just { inherit runTest; };
-  lefthook = pkgs.callPackage ./lefthook { inherit runTest; };
-  pre-commit = pkgs.callPackage ./pre-commit { inherit runTest; };
-  prettier = pkgs.callPackage ./prettier { inherit runTest; };
-}
+with pkgs.lib;
+let
+  # Build a list of all local directory names (test names)
+  tests = builtins.attrNames
+    (filterAttrs (n: v: v == "directory") (builtins.readDir ./.));
+
+  # Create a list of testName => import for each test
+  testsList = builtins.map
+    (p: {
+      "${p}" = import (builtins.toPath ./. + "/${p}") { inherit runTest; };
+    })
+    tests;
+
+  # Fold the list back into a single set
+  testsAttrs = fold (x: y: recursiveUpdate x y) { } testsList;
+in
+testsAttrs
