e410c698822cf1521be915a7fa50d5e4763811e0
Joshua Gilman <joshuagilman@gmail.com>
2022-06-09 16:18:00 -0700

    fix: prevents duplicating common shell code in makeAll

--- lib/make_all.nix
+++ lib/make_all.nix
@@ -5,8 +5,15 @@ all:
 with pkgs.lib;
 let
   result = builtins.map lib.make all;
+
+  # Only include common shell code once
+  common = (import ./hooks/common.nix);
+  shellHook = common + "\n" +
+    builtins.replaceStrings [ common ] [ "" ]
+      (concatStringsSep "\n"
+        (pkgs.lib.catAttrs "shellHook" result));
 in
 {
+  inherit shellHook;
   configs = catAttrs "configFile" result;
-  shellHook = concatStringsSep "\n" (pkgs.lib.catAttrs "shellHook" result);
 }
