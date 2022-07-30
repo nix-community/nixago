21272c35589c3460ae1515b59c69a8e356baab02
David Arnold <david.arnold@iohk.io>
2022-07-29 22:50:01 -0500

    fix: don't leave traces in the hook's environment (#46)
    fixes: #45

--- lib/make_all.nix
+++ lib/make_all.nix
@@ -6,10 +6,25 @@ with pkgs.lib;
 let
   result = builtins.map lib.make all;
 
+  prefixStringLines = prefix: str:
+    concatMapStringsSep "\n" (line: prefix + line) (splitString "\n" str);
+  indent = prefixStringLines "  ";
+
   # Only include common shell code once
   common = (import ./hooks/common.nix);
-  shellHook = common + "\nsource " +
-    (concatStringsSep "\nsource " (catAttrs "shellScript" result));
+  shellHook = ''
+    nixago() (
+      # Common shell code
+    ${indent common}
+
+      # Enable tracing if NIXAGO_TRACE==1
+      run_if_trace set -x
+      source ${concatStringsSep "\n  source " (catAttrs "shellScript" result)}
+      run_if_trace set +x
+    )
+    nixago
+    unset -f nixago
+  '';
 in
 {
   inherit shellHook;
